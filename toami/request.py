242f1e204d0ac1d25eb599748a9b3b0d72357d5e
Hirotaka Ichikawa <hichtakk@gmail.com>
2015-12-16 16:47:06 -0800

    add toami/request.py

--- toami/request.py
+++ toami/request.py
@@ -1,4 +1,36 @@
-class SNMPCommand(object):
+import abc
+
+class SNMPCommand(object, metaclass=abc.ABCMeta):
+
+    def __init__(self):
+        pass
+
+    def get(self):
+        pass
+
+    def set(self):
+        pass
+
+    def walk(self):
+        pass
+
+
+class SNMPCommandV1(SNMPCommand):
+
+    def __init__(self):
+        pass
+
+    def get(self):
+        pass
+
+    def set(self):
+        pass
+
+    def walk(self):
+        pass
+
+
+class SNMPCommandV2(SNMPCommandV1):
 
     def __init__(self):
         pass
