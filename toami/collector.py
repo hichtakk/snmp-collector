edaf993bc580d0db8a981fac0efbd0026ace5cdb
Hirotaka Ichikawa <hichtakk@gmail.com>
2015-12-16 16:46:50 -0800

    modify output style

--- toami/collector.py
+++ toami/collector.py
@@ -4,6 +4,7 @@ asyncronouse SNMP collector.
 
 import asyncio
 import time
+import functools
 
 from pysnmp.hlapi.asyncio import (SnmpEngine, getCmd, CommunityData,
                                   UdpTransportTarget, ContextData, ObjectType,
@@ -19,16 +20,16 @@ class SNMPCollector(object):
         self._semaphore = asyncio.Semaphore(semaphore)
         self._config = config
     
-    async def _collect(self, host, ois, community="public", port=161):
+    async def _collect(self, hostname, address, oids, community="public", port=161):
         snmpEngine = SnmpEngine()
         result = []
         with (await self._semaphore):
             errorIndication, errorStatus, errorIndex, varBinds = await getCmd(
                 snmpEngine,
                 CommunityData(community, mpModel=1),
-                UdpTransportTarget((host, port)),
+                UdpTransportTarget((address, port)),
                 ContextData(),
-                *[ObjectType(ObjectIdentity(o)) for o in ois]
+                *[ObjectType(ObjectIdentity(oid['oid'])) for oid in oids]
             )
             if errorIndication:
                 result.append({"status": "error",
@@ -42,10 +43,11 @@ class SNMPCollector(object):
             else:
                 for varBind in varBinds:
                     [oid, value] = [x for x in varBind]
-                    result.append({"host": host, "oid": str(oid),
+                    result.append({"host": hostname, "address": address, "oid": str(oid),
                                    "type": value.__class__.__name__,
                                    "value": str(value) if value else None,
-                                   "time": int(time.time())})
+                                   "time": int(time.time()),
+                                   "label": [x for x in oids if x['oid'] == str(oid)].pop()['description']})
         snmpEngine.transportDispatcher.closeDispatcher()
         return result
 
@@ -53,11 +55,11 @@ class SNMPCollector(object):
         loop = asyncio.get_event_loop()
         tasks = []
         for host in self._config["targets"]:
-            oids = [oid["oid"] for oid in host["oids"]]
             community = host["community"] if "community" in host else self._config["defaults"]["community"]
-            tasks.append(self._collect(host["address"], oids, community=community))
+            tasks.append(self._collect(host["name"], host["address"], host['oids'], community=community))
         result = loop.run_until_complete(asyncio.wait(tasks))
-        return result[0]
+        ret = functools.reduce(lambda a,b: a+b, [x.result() for x in result[0]])
+        return sorted(ret, key=lambda x:x['host'])
 
 
 if __name__ == '__main__':
